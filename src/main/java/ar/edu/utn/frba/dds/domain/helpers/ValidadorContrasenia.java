345058658800095b4489f0a58222029e52d56254
= <=>
2023-04-18 21:40:35 -0300

    agrgando clase implementacion validador de contrasenñas

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenia.java
@@ -0,0 +1,8 @@
+package ar.edu.utn.frba.dds.domain.helpers;
+
+public class ValidadorContrasenia implements ValidadorContrasenias {
+    private String URLPeoresClaves = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt";
+    public void validarPassword(String contrasenia) {
+
+    }
+}
---
+++ src/test/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContraseniaTests.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frba.dds.domain.helpers;
+
+public class ValidadorContraseniaTests {
+}
