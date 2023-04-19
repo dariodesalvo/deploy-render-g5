ffa6aec5f74e2674f28ce7ab7a7c836c062bda8c
mottato <mottato@frba.utn.edu.ar>
2023-04-18 22:13:55 -0300

    agrego try catch

--- src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenia.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenia.java
@@ -1,8 +1,16 @@
 package ar.edu.utn.frba.dds.domain.helpers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ValidadorContrasenia implements ValidadorContrasenias {
     private String URLPeoresClaves = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt";
-    public void validarPassword(String contrasenia) {
+    public void validarPassword(String contrasenia) throws MalformedURLException {
 
+        try {
+            URL oracle = new URL(URLPeoresClaves);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenias.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenias.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frba.dds.domain.helpers;
 
+import java.net.MalformedURLException;
+
 public interface ValidadorContrasenias {
 
-    void validarPassword(String contrasenia);
+    void validarPassword(String contrasenia) throws MalformedURLException;
 }
