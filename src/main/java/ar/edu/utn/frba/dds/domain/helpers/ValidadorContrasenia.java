1002ef660f4f218929d5cb38703bd4a2f64534d6
mottato <mottato@frba.utn.edu.ar>
2023-04-18 23:11:13 -0300

    agrego metodo para validar peores claves con tests

--- src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenia.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContrasenia.java
@@ -1,16 +1,34 @@
 package ar.edu.utn.frba.dds.domain.helpers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ValidadorContrasenia implements ValidadorContrasenias {
     private String URLPeoresClaves = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt";
-    public void validarPassword(String contrasenia) throws MalformedURLException {
 
+    public void validarPassword(String contrasenia) throws MalformedURLException {
+    }
+
+    public boolean esUnaDeLasPeoresClaves(String contrasenia) throws Exception {
         try {
-            URL oracle = new URL(URLPeoresClaves);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            String clave;
+            URL peoresClaves = new URL(URLPeoresClaves);
+            BufferedReader bufferReaderclaves = new BufferedReader(new InputStreamReader(peoresClaves.openStream()));
+            while ((clave = bufferReaderclaves.readLine()) != null) {
+                if (clave.equals(contrasenia)) {
+                    bufferReaderclaves.close();
+                    return true;
+                }
+            }
+            bufferReaderclaves.close();
+            return false;
+        } catch (MalformedURLException e ) {
+            throw new MalformedURLException("La URL no ha sido encontrada");
+        } catch (IOException e) {
+            throw new IOException("No se ha podido leer el archivo con las 10000 peores claves");
         }
     }
 }
--- src/test/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContraseniaTests.java
+++ src/test/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContraseniaTests.java
@@ -1,4 +1,29 @@
 package ar.edu.utn.frba.dds.domain.helpers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ValidadorContraseniaTests {
+    private ValidadorContrasenia validadorContraseña;
+    @BeforeEach
+    public void inicializar(){
+        validadorContraseña = new ValidadorContrasenia();
+    }
+
+    @Test
+    @DisplayName("qwerty es una de las peores claves")
+    public void unaClaveseEncuentraDentroDeLaListaDePeoresClaves() throws Exception {
+
+        Assertions.assertTrue(validadorContraseña.esUnaDeLasPeoresClaves("qwerty"));
+    }
+    @Test
+    @DisplayName("DDs2023 no es una de las peores claves")
+    public void unaClaveseNoEncuentraDentroDeLaListaDePeoresClaves() throws Exception {
+
+        Assertions.assertFalse(validadorContraseña.esUnaDeLasPeoresClaves("DDs2023"));
+    }
+
 }
+
