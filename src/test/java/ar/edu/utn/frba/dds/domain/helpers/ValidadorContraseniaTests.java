d5b0454f43a775a7e7c014bf0ba139d386c8c5bb
mottato <mottato@frba.utn.edu.ar>
2023-04-23 22:15:36 -0300

    arreglo tests de validador de contraseña

--- src/test/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContraseniaTests.java
+++ src/test/java/ar/edu/utn/frba/dds/domain/helpers/ValidadorContraseniaTests.java
@@ -6,53 +6,49 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidadorContraseniaTests {
-  private ValidadorContrasenia validadorContraseña;
 
-  @BeforeEach
-  public void inicializar() {
-    validadorContraseña = new ValidadorContrasenia();
-  }
+
 
   @Test
   @DisplayName("qwerty es una de las peores claves")
   public void unaClaveseEncuentraDentroDeLaListaDePeoresClaves() throws Exception {
 
-    Assertions.assertTrue(validadorContraseña.esUnaDeLasPeoresClaves("qwerty"));
+    Assertions.assertTrue(ValidadorContrasenia.esUnaDeLasPeoresClaves("qwerty"));
   }
 
   @Test
   @DisplayName("DDs2023 no es una de las peores claves")
   public void unaClaveseNoEncuentraDentroDeLaListaDePeoresClaves() throws Exception {
 
-    Assertions.assertFalse(validadorContraseña.esUnaDeLasPeoresClaves("DDs2023"));
+    Assertions.assertFalse(ValidadorContrasenia.esUnaDeLasPeoresClaves("DDs2023"));
   }
 
   @Test
   @DisplayName("Password1 no cumple con guia NIST")
   public void unaClaveNoesValidaNist() throws Exception {
 
-    Assertions.assertFalse(validadorContraseña.esValidaNist("Password1"));
+    Assertions.assertFalse(ValidadorContrasenia.esValidaNist("Password1"));
   }
 
   @Test
   @DisplayName("Password1* cumple con guia NIST")
   public void unaClaveEsValidaNist() throws Exception {
 
-    Assertions.assertTrue(validadorContraseña.esValidaNist("Password1*"));
+    Assertions.assertTrue(ValidadorContrasenia.esValidaNist("Password1*"));
   }
 
   @Test
   @DisplayName("PasSSSword1* es una contraseña valida")
   public void unaClaveEsValida() throws Exception {
 
-    Assertions.assertTrue(validadorContraseña.laContraseniaEsValida("PasSSSword1*"));
+    Assertions.assertTrue(ValidadorContrasenia.laContraseniaEsValida("PasSSSword1*"));
   }
 
   @Test
   @DisplayName("pasSSword no es una contraseña valida")
   public void unaClaveEsInvalida() throws Exception {
 
-    Assertions.assertFalse(validadorContraseña.laContraseniaEsValida("pasSSword"));
+    Assertions.assertFalse(ValidadorContrasenia.laContraseniaEsValida("pasSSword"));
   }
 
 
