4715699cff5da4603e1e18394855b5032b56d498
Dario Ezequiel Desalvo <dariodesalvo@gmail.com>
2023-06-04 16:25:54 -0700

    Creacion de clase entidad y su relacion con linea.

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/entidades/Entidad.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.domain.entidades;
+
+import ar.edu.utn.frba.dds.domain.comunidades.Usuario;
+
+import java.util.Collection;
+
+public class Entidad {
+    private String leyenda;
+    private Collection<Usuario> followers;
+}
