03fcfe8d05509e67f0b5fd56605872cc3e277307
MauroDeMarco <maurodemarco999@gmail.com>
2023-07-12 19:49:42 -0300

    Se crea clase incidente y modifica Miembro

--- src/main/java/ar/edu/utn/frba/dds/domain/comunidades/Miembro.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/comunidades/Miembro.java
@@ -1,7 +1,13 @@
 package ar.edu.utn.frba.dds.domain.comunidades;
 
+import ar.edu.utn.frba.dds.domain.entidades.Establecimiento;
+import ar.edu.utn.frba.dds.domain.incidentes.Incidente;
+import ar.edu.utn.frba.dds.domain.servicios.Servicio;
+import ar.edu.utn.frba.dds.domain.comunidades.Comunidad
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -9,11 +15,16 @@ public class Miembro extends RolesUsuario {
 
     private String nombre;
     private String apellido;
+    private List<Comunidad> comunidades;
+    private List<Servicio> serviciosDeInteres;
+    private
 
     public Miembro(String nombre, String apellido){
         this.nombre=nombre;
         this.apellido=apellido;
     }
 
+    public void abrirIncidente(Servicio servicio){}
+    public void cerrarIncidente(Servicio servicio){}
 
 }
