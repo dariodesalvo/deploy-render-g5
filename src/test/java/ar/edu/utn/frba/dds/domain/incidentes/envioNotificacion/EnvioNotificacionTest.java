99570f9243cfff9597eb8597736b8d7461f45346
MauroDeMarco <maurodemarco999@gmail.com>
2023-08-07 21:45:32 -0300

    Se modifica test de envioNotificacion por Whatsapp

--- src/test/java/ar/edu/utn/frba/dds/domain/incidentes/envioNotificacion/EnvioNotificacionTest.java
+++ src/test/java/ar/edu/utn/frba/dds/domain/incidentes/envioNotificacion/EnvioNotificacionTest.java
@@ -4,17 +4,28 @@ import ar.edu.utn.frba.dds.domain.comunidades.Comunidad;
 import ar.edu.utn.frba.dds.domain.comunidades.Miembro;
 import ar.edu.utn.frba.dds.domain.comunidades.Usuario;
 import ar.edu.utn.frba.dds.domain.incidentes.mediosNotificacion.Email;
+import ar.edu.utn.frba.dds.domain.incidentes.mediosNotificacion.MedioNotificable;
 import ar.edu.utn.frba.dds.domain.incidentes.mediosNotificacion.Whatsapp;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import org.apache.commons.mail.EmailException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 public class EnvioNotificacionTest {
     private Usuario caro = new Usuario("rinaldicarolina@hotmail.com","Carolina1*" );
     private Miembro miembro = new Miembro("Carolina", "Rinaldi");
     private Miembro miembro2 = new Miembro("Caro","R");
     private Comunidad comunidad = new Comunidad("PMR");
+
+
+
 
     public EnvioNotificacionTest() throws Exception {
     }
@@ -36,7 +47,11 @@ public class EnvioNotificacionTest {
     @Test
     @DisplayName("Se envia notificacion por whatsapp a miembro")
     public void enviarWhatsapp() throws EmailException {
+
+        Whatsapp medio = mock(Whatsapp.class);
+        doNothing().when(medio).enviarNotificacion();
         miembro.getMedioComunicacionPreferido().enviarNotificacion();
+        verify(medio, Mockito.times(1)).enviarNotificacion();
 
     }
 
