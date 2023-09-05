f8162a3227d75d505b9bd0b4578024892184f3eb
Dario Ezequiel Desalvo <dariodesalvo@gmail.com>
2023-09-04 22:53:09 -0300

    Lógica de notificaciones pendientes

--- src/main/java/ar/edu/utn/frba/dds/domain/incidentes/TurnoDeNotificaciones/TurnoNotificacion.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/incidentes/TurnoDeNotificaciones/TurnoNotificacion.java
@@ -2,14 +2,15 @@ package ar.edu.utn.frba.dds.domain.incidentes.TurnoDeNotificaciones;
 
 import ar.edu.utn.frba.dds.domain.comunidades.Miembro;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 @Setter
 @Getter
 public abstract class TurnoNotificacion {
-    protected List<Miembro> miembros;
-    protected List<String> notificaciones;
+    protected List<Miembro> miembros = new ArrayList<>();
+    protected List<String> notificaciones = new ArrayList<>();
 
     public void agregarNotificacion(String notificacion){
         notificaciones.add(notificacion);
@@ -19,4 +20,7 @@ public abstract class TurnoNotificacion {
         notificaciones.remove(notificacion);
     }
 
+    public void agregarMiembro(Miembro miembro){
+        miembros.add(miembro);
+    }
 }
---
+++ src/test/java/ar/edu/utn/frba/dds/domain/incidentes/envioNotificacion/CronNotificadorTest.java
@@ -0,0 +1,41 @@
+package ar.edu.utn.frba.dds.domain.incidentes.envioNotificacion;
+
+import ar.edu.utn.frba.dds.domain.comunidades.Miembro;
+import ar.edu.utn.frba.dds.domain.comunidades.Usuario;
+import ar.edu.utn.frba.dds.domain.incidentes.CronNotificador;
+import ar.edu.utn.frba.dds.domain.incidentes.Notificador;
+import ar.edu.utn.frba.dds.domain.incidentes.TurnoDeNotificaciones.TurnoM;
+import ar.edu.utn.frba.dds.domain.incidentes.TurnoDeNotificaciones.TurnoNotificacion;
+import ar.edu.utn.frba.dds.domain.incidentes.mediosNotificacion.adapterImpl.NotificarPorEmail;
+import ar.edu.utn.frba.dds.domain.incidentes.mediosNotificacion.sender.EmailSender;
+
+
+public class CronNotificadorTest {
+    public static void main(String arglist[]) throws Exception {
+        // ejemplo de uso
+        // se inicializan todas las instancias minimas necesarias para comunicar una notificacion programada
+        Notificador notificador = new Notificador();
+        TurnoNotificacion turno = new TurnoM();
+        Usuario caro = new Usuario("ddesalvo@frba.utn.edu.ar", "Password1*");
+        Miembro miembro = new Miembro("caro", "rinaldi");
+        caro.setRol(miembro);
+        miembro.setEmail(caro.getEmail());
+        NotificarPorEmail adapterEmail = new NotificarPorEmail();
+        EmailSender emailSender = new EmailSender(adapterEmail);
+        miembro.setMedioNotificacionPreferido(emailSender);
+        turno.agregarMiembro(miembro);
+        String mensaje = "Este es un mensaje enviado de prueba usando crontask";
+        turno.agregarNotificacion(mensaje);
+
+        //se inicia el cronNotificador que en este caso corre cada 10 minutos.
+        CronNotificador cronNotificador = new CronNotificador();
+        cronNotificador.setNotificador(notificador);
+        cronNotificador.setTurno(turno);
+        cronNotificador.ejecutar();
+
+        // se simula un nuevo mensaje de incidente
+        mensaje = "10 minutos despues este es un mensaje enviado de prueba usando crontask";
+        turno.agregarNotificacion(mensaje);
+
+    }
+}
