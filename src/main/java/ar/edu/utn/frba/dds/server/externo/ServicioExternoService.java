bbcccb14712b2ba7ba693d998a315ce7677ecb29
Carolina <crinaldi@frba.utn.edu.ar>
2023-11-01 01:47:04 -0300

    integracion con Servicio 3 funcionando

--- src/main/java/ar/edu/utn/frba/dds/server/externo/ServicioExternoService.java
+++ src/main/java/ar/edu/utn/frba/dds/server/externo/ServicioExternoService.java
@@ -1,11 +1,10 @@
 package ar.edu.utn.frba.dds.server.externo;
 
-import ar.edu.utn.frba.dds.models.externo.ExternoRequest;
-import ar.edu.utn.frba.dds.models.externo.ExternoResponse;
+import ar.edu.utn.frba.dds.serviceIntegration.servicio3.entities.ExternoRequest;
+import ar.edu.utn.frba.dds.serviceIntegration.servicio3.entities.ExternoResponse;
 import retrofit2.Call;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface ServicioExternoService {
     @POST("entidades")
--- src/main/java/ar/edu/utn/frba/dds/serviceIntegration/servicio3/entities/ExternoResponse.java
+++ src/main/java/ar/edu/utn/frba/dds/serviceIntegration/servicio3/entities/ExternoResponse.java
@@ -1,4 +1,4 @@
-package ar.edu.utn.frba.dds.models.externo;
+package ar.edu.utn.frba.dds.serviceIntegration.servicio3.entities;
 
 import lombok.Getter;
 import lombok.Setter;
