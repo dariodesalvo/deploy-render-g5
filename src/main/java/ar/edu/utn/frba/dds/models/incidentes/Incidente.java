b718a492f339f1a8e9e906790d7a93b7e7a62b4d
Carolina <crinaldi@frba.utn.edu.ar>
2023-12-06 18:36:59 -0300

    se persiste la clase incidente y se agrega converter de LocalDateTime

--- src/main/java/ar/edu/utn/frba/dds/models/incidentes/Incidente.java
+++ src/main/java/ar/edu/utn/frba/dds/models/incidentes/Incidente.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frba.dds.models.incidentes;
 
 import ar.edu.utn.frba.dds.models.comunidades.Comunidad;
 import ar.edu.utn.frba.dds.models.comunidades.Miembro;
+import ar.edu.utn.frba.dds.models.converters.LocalDateTimeAttributeConverter;
 import ar.edu.utn.frba.dds.models.incidentes.mensajes.AperturaIncidente;
 import ar.edu.utn.frba.dds.models.incidentes.mensajes.CierreIncidente;
 import ar.edu.utn.frba.dds.models.incidentes.mensajes.Mensaje;
@@ -10,23 +11,52 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.mail.EmailException;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-//TODO persistir
+@Entity
+@Table(name = "incidente")
 public class Incidente {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "servicio_id", referencedColumnName = "id")
     private Servicio servicio;
+
     //private Establecimiento establecimiento; -> no deberia ir en servicio?
+
+    @OneToOne
+    @JoinColumn(name = "miembro_id", referencedColumnName = "id")
     private Miembro abiertoPor;
+
+    @OneToOne
+    @JoinColumn(name = "miembro_id", referencedColumnName = "id")
     private Miembro cerradoPor;
+
+    @ManyToOne
+    @JoinColumn(name = "comunidad_id", referencedColumnName = "id")
     private Comunidad comunidad;
+
+    @Column(name = "estado")
     private Boolean estado;
+
+    @Column(name = "observaciones")
     private String observaciones;
+
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    @Column(name = "fechaApertura")
     private LocalDateTime fechaApertura;
+
+    @Convert(converter = LocalDateTimeAttributeConverter.class)
+    @Column(name = "fechaCierre")
     private LocalDateTime fechaCierre;
+
+    @Transient
     private Notificador notificador;
 
     public Incidente(Servicio servicio, Miembro abiertoPor, Comunidad comunidad, String observaciones) throws EmailException {
