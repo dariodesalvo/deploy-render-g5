da8b3934fdd0e4220287f281259563f620d07d6a
Carolina <crinaldi@frba.utn.edu.ar>
2024-02-10 18:09:11 -0300

    Se elimina campo Ubicacion en pantalla de perfil y la logica de edit en UsuarioController

--- src/main/java/ar/edu/utn/frba/dds/controllers/UsuarioController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/UsuarioController.java
@@ -161,57 +161,6 @@ public class UsuarioController extends Controller implements ICrudViewsHandler {
     @Override
     public void edit(Context context) throws IOException {
 
-
-        Usuario usuario = (Usuario) repositorioDeUsuarios.buscar(Long.parseLong(context.sessionAttribute("usuario_id")));
-        ListadoDeProvincias listadoDeProvincias = servicioGeoref.listadoDeProvincias();
-        Miembro miembro = (Miembro) usuario.getRol();
-
-        miembro.setApellido(context.formParam("apellido"));
-        miembro.setNombre(context.formParam("nombre"));
-        miembro.setCelular(context.formParam("celular"));
-        miembro.setIdProvincia(Long.parseLong(context.formParam("idProvincia")));
-        miembro.setIdMunicipio(Integer.parseInt(context.formParam("idMunicipio")));
-
-        Map<String, Object> model = new HashMap<>();
-
-        //carga el selected
-        if (!Objects.equals(context.pathParam("idProvincia"), "")) {
-            ListadoDeProvincias listadoDeProvinciasPorID = servicioGeoref.listadoDeProvinciasPorID(Integer.parseInt(context.pathParam("idProvincia")));
-            model.put("provincia", listadoDeProvinciasPorID.provincias.get(0));
-        }
-
-        ListadoDeMunicipios listadoDeMunicipios = null;
-        //carga todos los municipios que usa
-        if (!Objects.equals(context.pathParam("idProvincia"), "")) {
-            listadoDeMunicipios = servicioGeoref.listadoDeMunicipiosDeProvincia(Integer.parseInt(context.pathParam("idProvincia")));
-        }
-
-        if (!Objects.equals(context.formParam("idMunicipio"), null)) {
-            miembro.setIdMunicipio(Integer.parseInt(context.formParam("idMunicipio")));
-        }
-
-        //carga el selected de municipio
-        if (!Objects.equals(miembro.getIdMunicipio(), 0)) {
-            ListadoDeMunicipios listadoDeMunicipiosPorID = servicioGeoref.listadoDeMunicipiosPorID(miembro.getIdMunicipio());
-            model.put("municipio", listadoDeMunicipiosPorID.municipios.get(0));
-            /*
-            ListadoDeProvincias listadoDeProvinciasPorID = servicioGeoref.listadoDeProvinciasPorID(Integer.parseInt(establecimiento.getIdProvincia().toString()));
-            model.put("municipio", listadoDeProvinciasPorID.provincias.get(0));
-        */
-        }
-        model.put("municipios", listadoDeMunicipios.municipios);
-        model.put("provincias", listadoDeProvincias.provincias);
-        /* desde aca */
-        model.put("email", context.sessionAttribute("email"));
-        model.put("tipo_rol", context.sessionAttribute("tipo_rol"));
-        model.put("usuario_id", context.sessionAttribute("usuario_id"));
-        model.put("MiembroAdmin", context.sessionAttribute("MiembroAdmin"));
-        //model.put("municipio", usuario.getMunicipio());
-        model.put("miembro", usuario.getRol());
-        model.put("municipio", miembro.getIdMunicipio());
-        model.put("provincia", miembro.getIdProvincia());
-        model.put("Miembro", context.sessionAttribute("Miembro"));
-        context.render("/login/perfil.hbs", model);
     }
 
     @Override
